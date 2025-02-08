c0694b94e8aefa9c9e55c21e4d32461ad63a4ad9
ak9099 <ak9931927833@gmail.com>
2025-02-08 11:53:38 +0530

    utils added

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+app.use(cors({
+  origin:process.env.CORS_ORIGIN,
+  credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))  //used for getting the json file
+
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public")) //used for storing the image,pdf,etc
+
+app.use(cookieParser())  //user ke browser ka cookies access and set kar pau and then crud operation perform kar saku
+
+
+export default app
